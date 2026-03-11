aaabdb7ef3c289e24a375689c3e2312a6d97243d
srikar-04 <srikar.code01@gmail.com>
2026-03-11 22:27:01 +0530

    initialized prisma with nenon db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,1 @@
-import { readFile } from "fs";
-
-const my_test_function = (name: string) => {
-  const unusedVar = 10;
-  console.log("Hello " + name);
-  return "formatted";
-};
-
-my_test_function("world");
+console.log("hello world");
