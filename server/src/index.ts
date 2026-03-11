7d6f68cd2c35f35a8fe82a103ed3aecbdf3878fb
srikar-04 <srikar.code01@gmail.com>
2026-03-11 20:28:02 +0530

    checking working of prettier and eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world")
+import { readFile } from "fs"
+
+const my_test_function = ( name:string ) =>{
+const unusedVar = 10;
+  console.log("Hello " + name)
+    return 'formatted'
+}
+
+my_test_function( "world")
