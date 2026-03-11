e71995bf51c61721e99d5f7a7d98617ce8a38efb
srikar-04 <srikar.code01@gmail.com>
2026-03-11 21:05:18 +0530

    Husky test

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs";
 
 const my_test_function = (name: string) => {
   const unusedVar = 10;
-       console.log("Hello " + name);
+  console.log("Hello " + name);
   return "formatted";
 };
 
