651594ab5fade535e320088f196a854ddc59483e
srikar-04 <srikar.code01@gmail.com>
2026-03-12 17:18:13 +0530

    initialized vite project and integrated tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <main className="w-full h-screen bg-zinc-800 text-white flex items-center justify-center">
+        <div>
+          hello world
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
